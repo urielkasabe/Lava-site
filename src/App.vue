5067fbfb4d641774ebff80d218f0edae5c2d42d8
Uriel Kasabe <urielkasabe5@gmail.com>
2023-05-18 15:06:38 +0300

    added loading animation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="loading-animation">
+    <img class="imagen" src="../images/lavabarwhitesmall.png" alt="lava-logo" />
+  </div>
+  <h1 class="loading-h1">העמוד בטעינה אנא המתן...</h1>
   <header>
     <menu-list></menu-list>
   </header>
@@ -47,11 +51,29 @@ export default {
     ContactUs,
     TheFooter,
   },
+  mounted() {
+    window.addEventListener("load", this.hideLoadingAnimation);
+  },
+  beforeUnmount() {
+    window.removeEventListener("load", this.hideLoadingAnimation);
+  },
+  methods: {
+    hideLoadingAnimation() {
+      const loadingAnimation = document.querySelector(".loading-animation");
+      const loadingH1 = document.querySelector(".loading-h1");
+      if (loadingAnimation) {
+        loadingAnimation.remove();
+      }
+      if (loadingH1) {
+        loadingH1.remove();
+      }
+    },
+  },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Heebo:wght@400;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Heebo:wght@400;700&display=swap");
 
 * {
   box-sizing: border-box;
@@ -62,7 +84,7 @@ html {
 }
 
 body {
-  font-family: 'Heebo', sans-serif;
+  font-family: "Heebo", sans-serif;
   margin: 0;
   padding: 0;
 }
@@ -85,13 +107,77 @@ hr {
   width: 70%;
 }
 
+.loading-animation {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: black;
+  z-index: 9999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.loading-animation:after {
+  content: "";
+  display: block;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: 4px solid white;
+  border-color: white transparent white transparent;
+  animation: spin 1s linear infinite;
+}
+
+.imagen {
+  height: 5rem;
+  width: 5rem;
+}
+
+.loading-h1{
+  display: block;
+  position: fixed;
+  top: 55%;
+  left: 50%;
+  transform: translateX(-50%);
+  margin: 1rem auto;
+  text-align: center;
+  color: white;
+  direction: rtl;
+  font-size: 2rem;
+  z-index: 9999;
+  overflow: hidden;
+  white-space: nowrap;
+  animation: typing 5s steps(60, end) infinite;
+}
+
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
 @media (max-width: 768px) {
-  html{
+  html {
     width: 100%;
   }
 
   hr {
-  margin: 5rem auto 0 auto;
-}
+    margin: 5rem auto 0 auto;
+  }
 }
 </style>
